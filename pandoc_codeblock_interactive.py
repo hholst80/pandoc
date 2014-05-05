5892f1431c3cfe1064569a67b8743cccb21fa495
Henrik Holst <henrik.holst@dice.se>
2014-05-05 17:58:41 +0200

    Cleanup hanging prompt

--- pandoc_codeblock_interactive.py
+++ pandoc_codeblock_interactive.py
@@ -14,24 +14,23 @@ def octave(key, value, format, meta):
       return
     options = value[0][1]
     text = value[1]
+    prg = None
+    cmd = None
     if 'octave' in options and 'interactive' in options:
-        f = file('stdin.txt','w')
-        f.write(text)
-        f.close()
-        subprocess.call('env LD_PRELOAD=./loginteractive.so STDIN=stdin.txt STDOUT=stdout.txt octave -qi --traditional > /dev/null', shell=True, bufsize=1)
-        f = open('stdout.txt','r')
-        text = f.read()
-        f.close()
-        return CodeBlock(("",["octave", "interactive"],[]),text)
+        prg = 'octave'
+        cmd = 'env LD_PRELOAD=./loginteractive.so STDIN=stdin.txt STDOUT=stdout.txt octave -qi --traditional > /dev/null'
     elif 'python' in options and 'interactive' in options:
+        prg = 'python'
+        cmd = 'env LD_PRELOAD=./loginteractive.so STDIN=stdin.txt STDOUT=stdout.txt python -i > /dev/null'
+    if prg and cmd:
         f = file('stdin.txt','w')
         f.write(text)
         f.close()
-        subprocess.call('env LD_PRELOAD=./loginteractive.so STDIN=stdin.txt STDOUT=stdout.txt python -i > /dev/null', shell=True, bufsize=1)
+        subprocess.call(cmd, shell=True, bufsize=1)
         f = open('stdout.txt','r')
-        text = f.read()
+        text = "\n".join(f.read().split("\n")[0:-1])
         f.close()
-        return CodeBlock(("",["python", "interactive"],[]),text)
+        return CodeBlock(("",[prg, "interactive"],[]),text)
 
 if __name__ == "__main__":
         toJSONFilter(octave)
